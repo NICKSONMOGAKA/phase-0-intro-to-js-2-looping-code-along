f2b8a3a2eba9c187939cd00d938dc338f7731440
+ <nickson.mogaka@student.moringaschool.com>
2024-06-21 13:15:57 +0300

    modify

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+// Function to generate thank you messages
+function writeCards(namesArray, eventName) {
+    let thankYouMessages = [];
+    for (let i = 0; i < namesArray.length; i++) {
+      thankYouMessages.push(`Thank you, ${namesArray[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return thankYouMessages;
+  }
+  
+  // Function to count down from a number to zero, logging each number
+  function countDown(number) {
+    while (number >= 0) {
+      console.log(number);
+      number--;
+    }
+  }
+  
+  module.exports = {
+    writeCards, // <-- Corrected typo: removed extra characters
+    countDown,
+  };
+  
